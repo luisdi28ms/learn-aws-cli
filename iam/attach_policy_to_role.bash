84f7e862ed800ffa633281d57d4edde2bb53c4d6
Luis Monge <luisdi28ms@gmail.com>
2026-07-06 17:37:55 -0600

    IAM - Deny Policies Added a deny-all policy to the readonly role to stop the EC2 instance from reading other services

--- iam/attach_policy_to_role.bash
+++ iam/attach_policy_to_role.bash
@@ -1,3 +1,7 @@
+# aws iam attach-role-policy \
+#   --role-name patientping-ec2-readonly-role \
+#   --policy-arn "arn:aws:iam::700694289578:policy/patientping-ec2-readonly"
+
 aws iam attach-role-policy \
   --role-name patientping-ec2-readonly-role \
-  --policy-arn "arn:aws:iam::700694289578:policy/patientping-ec2-readonly"
+  --policy-arn "arn:aws:iam::700694289578:policy/patientping-deny-all"
--- iam/create_policy.bash
+++ iam/create_policy.bash
@@ -1,3 +1,6 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-aws iam create-policy --policy-name patientping-ec2-readonly \
-  --policy-document "file://${SCRIPT_DIR}/policies/patientping_ec2_readonly.json"
+# aws iam create-policy --policy-name patientping-ec2-readonly \
+#   --policy-document "file://${SCRIPT_DIR}/policies/patientping_ec2_readonly.json"
+#
+aws iam create-policy --policy-name patientping-deny-all \
+  --policy-document "file://${SCRIPT_DIR}/policies/patientping_deny_all.json"
